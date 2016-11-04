3c1f6f48533ae142a1662c12c5bd257f3a5d7948
Daniel França <daniel.franca@gmail.com>
2016-11-05 00:38:09 +0100

    Set languages to only english for now

--- tintz/tintz/settings.py
+++ tintz/tintz/settings.py
@@ -129,6 +129,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LANGUAGES = [
+    ('en', 'English'),
+]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
